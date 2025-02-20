cb4ec7f74ef426d42c53c2e3b97ef1a9f93956f2
Miguel Angel Sosa <gravitones@hotmail.com>
2025-02-19 18:22:18 -0600

    uses Arguments matchers

--- src/test/java/com/kosmostecnologia/airdnd/services/RoomServicesTest.java
+++ src/test/java/com/kosmostecnologia/airdnd/services/RoomServicesTest.java
@@ -3,30 +3,37 @@ package com.kosmostecnologia.airdnd.services;
 import com.kosmostecnologia.airdnd.dto.RoomDto;
 import com.kosmostecnologia.airdnd.repositories.RoomRepository;
 import com.kosmostecnologia.airdnd.utils.DataDummy;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class RoomServicesTest {
 
-    private RoomService roomService;
+    @Mock
     private RoomRepository roomRepositoryMock;
 
-    @BeforeEach
-    void init (){
-        this.roomRepositoryMock = mock(RoomRepository.class);
-        this.roomService=new RoomService(roomRepositoryMock);
-    }
+    @InjectMocks
+    private RoomService roomService;
+
+//    @BeforeEach
+//    void init (){
+//        this.roomRepositoryMock = mock(RoomRepository.class);
+//        this.roomService=new RoomService(roomRepositoryMock);
+//    }
 
     @Test
     @DisplayName("Should get all rooms available in room repository")
     void findAllAvailableRooms(){
-        when(roomRepositoryMock.findAll()).thenReturn(DataDummy.DEFAULTS_ROOMS);
+        when(roomRepositoryMock.findAll()).thenReturn(DataDummy.DEFAULT_ROOMS);
         int expected = 3;
         List<RoomDto> result = this.roomService.findAllAvailableRooms();
         assertEquals(expected,result.size());
--- src/test/java/com/kosmostecnologia/airdnd/utils/DataDummy.java
+++ src/test/java/com/kosmostecnologia/airdnd/utils/DataDummy.java
@@ -1,9 +1,10 @@
 package com.kosmostecnologia.airdnd.utils;
 
+import com.kosmostecnologia.airdnd.dto.BookingDto;
 import com.kosmostecnologia.airdnd.dto.RoomDto;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 public class DataDummy {
@@ -11,7 +12,7 @@ public class DataDummy {
     private DataDummy(){
 
     }
-    public static final Map<RoomDto,Boolean> DEFAULTS_ROOMS = Map.ofEntries(
+    public static final Map<RoomDto,Boolean> DEFAULT_ROOMS = Map.ofEntries(
 
             Map.entry(new RoomDto("A", 2),true),
             Map.entry(new RoomDto("B", 2),true),
@@ -20,5 +21,20 @@ public class DataDummy {
             Map.entry(new RoomDto("E", 2),false),
             Map.entry(new RoomDto("F", 3),false)
    );
+
+    public static final List<RoomDto> DEFAULT_ROOMS_LIST = List.of(
+            new RoomDto("A",2),
+            new RoomDto("B",2),
+            new RoomDto("C",3),
+            new RoomDto("D",2),
+            new RoomDto("E",2),
+            new RoomDto("F",3)
+    );
+
+    public static final BookingDto DEFAULT_BOOKING_REQ_1 = new BookingDto("18318", LocalDate.of(2023,6,10),
+            LocalDate.of(2023,6,20),2,false);
+
+    public static final BookingDto DEFAULT_BOOKING_REQ_2 = new BookingDto("3784193", LocalDate.of(2023,6,10),
+            LocalDate.of(2023,6,26),2,false);
 
 }
